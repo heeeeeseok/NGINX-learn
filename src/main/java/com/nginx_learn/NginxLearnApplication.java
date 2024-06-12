8bb5ea3c89879137ac7aef295ff4953fe08c57be
hseoky desktop <skdlzl753@naver.com>
2024-06-12 22:22:36 +0900

    init

---
+++ src/main/java/com/nginx_learn/NginxLearnApplication.java
@@ -0,0 +1,13 @@
+package com.nginx_learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NginxLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NginxLearnApplication.class, args);
+	}
+
+}
